dbfb794b6667c0ebabba56b5fd1cae4b88873e0b
Thibaud Guillot <thibaud.guillot@biblibre.com>
2025-09-26 16:18:22 +0200

    Add medias info on $039

--- src/MappingClass/Bokeh/BokehStandard.php
+++ src/MappingClass/Bokeh/BokehStandard.php
@@ -26,7 +26,7 @@ class BokehStandard extends UnimarcStandard
         return 'Bokeh Standard';
     }
 
-    public function getXmlFile($resources) : DOMDocument
+    public function getXmlFile($resources): DOMDocument
     {
         $dom = new DOMDocument('1.0', 'UTF-8');
         $collection = $dom->createElementNS('http://www.loc.gov/MARC21/slim', 'xlms:collection');
@@ -90,6 +90,25 @@ class BokehStandard extends UnimarcStandard
 
         $fieldsMapping = $this->addResourceTemplateMap($fieldsMapping, $resource);
         $fieldsMapping = $this->addResourceClassMap($fieldsMapping, $resource);
+
+        if ($resourceType === 'o:Item') {
+            $medias = $resource->media();
+            $publicMediaIds = [];
+            if (isset($medias)) {
+                foreach ($medias as $media) {
+                    if ($media->isPublic()) {
+                        $publicMediaIds[] = $media->id();
+                    }
+                }
+            }
+            $fieldsMapping['039'] = [];
+            if (!empty($publicMediaIds)) {
+                $fieldsMapping['039']['a'] = implode(',', $publicMediaIds);
+            }
+            $fieldsMapping['039']['b'] = "média";
+            $fieldsMapping['039']['c'] = !empty($medias) ? 'oui' : 'non';
+            $fieldsMapping['039']['d'] = !empty($publicMediaIds) ? 'oui' : 'non';
+        }
 
         if ($resourceType === 'o:Media') {
             $fieldsMapping['049'] = [
